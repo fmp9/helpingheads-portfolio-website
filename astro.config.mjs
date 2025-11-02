b709077d57672895ad93a3f8f0669cb1dc426bc5
fmp9 <flo99p@gmx.at>
2025-11-02 14:33:27 +0100

    config file for github hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
+// astro.config.ts
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  site: "https://www.helpingheads.at/",
+  site: "https://fmp9.github.io/helpingheads-portfolio-website/",
+  base: "/helpingheads-portfolio-website/",
+  vite: { plugins: [tailwindcss()] },
   integrations: [sitemap(), react()],
 });
